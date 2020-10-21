7b04da58108ccdcebe3848acc4796a57832ddc03
Marcus <Marcus@DATOR>
2020-10-21 11:37:56 +0200

    Add project files.

---
+++ StorageAccountExample/Program.cs
@@ -0,0 +1,39 @@
+﻿using StorageAccountExample.Services;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace StorageAccountExample
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var connectionString = "DefaultEndpointsProtocol=https;AccountName=godlike;AccountKey=7/YNhTvUi1d0/q52bdBADb+/fDfLDiit7SCPlwNeo5voaNu8J1WmfIcyN9SUuiWGTSf7Oa7sbE3zAdYZs1yzmQ==;EndpointSuffix=core.windows.net";
+            var containerName = "godlikes";
+
+            var fileName = $"myfile-{Guid.NewGuid()}.txt";
+            var content = "this is the content of the file ";
+            var filePath = Path.Combine(@"d:\WIN20\", fileName);
+            var downloadPath = Path.Combine(@"d:\WIN20\Download\",fileName);
+
+            Console.WriteLine("Initializing Storage Account With ContainerName:" + containerName );
+            await  StorageService.InitilizeStorageAsync(connectionString, containerName);
+
+            Console.WriteLine("Creating and Writeing content in file: " + filePath);
+            await StorageService.WriteToFileAsync(filePath, content);
+
+            Console.WriteLine("Uploading file to Azure Storage Blob in container " + containerName);
+            await StorageService.UploadFileAsync(filePath);
+
+            Console.WriteLine("Downloading file from Azure Storage Blob to: " + Path.GetDirectoryName(downloadPath));
+            await StorageService.DownloadFileAsync(downloadPath);
+
+            Console.WriteLine("Reading content from file: " + downloadPath);
+            Console.WriteLine(await StorageService.ReadDownloadFileAsync(downloadPath));
+
+
+
+        }
+    }
+}
---
+++ StorageAccountExample/Services/StorageService.cs
@@ -0,0 +1,147 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StorageAccountExample.Services
+{
+    public static class StorageService
+    {
+
+        private static BlobContainerClient _containerClient { get; set; }
+        private static BlobClient _blobClient { get; set; }
+
+
+        public static async Task InitilizeStorageAsync(string connectionString, string containerName, bool uniqueName = false)
+        {
+            if (uniqueName)
+            {
+                containerName = $"{containerName}-{Guid.NewGuid()}";
+            }
+
+
+            try
+            {
+                BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
+
+                try
+                {
+                    _containerClient = await blobServiceClient.CreateBlobContainerAsync(containerName);
+                }
+                catch 
+                {
+                    try
+                    {
+                        _containerClient = blobServiceClient.GetBlobContainerClient(containerName);
+                    }
+                    catch 
+                    {
+                        Console.WriteLine("Failed Third Initilize");
+
+                    }
+                    
+                }
+            }
+            catch 
+            {
+                Console.WriteLine("Failed First Initilize");
+                
+            }
+
+
+
+        }
+
+        public static async Task WriteToFileAsync(string @filePath, string content)
+        {
+            try
+            {
+                if (!Directory.Exists(Path.GetDirectoryName(filePath)))
+                {
+                    Directory.CreateDirectory(Path.GetDirectoryName(filePath));
+                }
+
+
+                await File.WriteAllTextAsync(filePath, content);
+            }
+            catch 
+            {
+                Console.WriteLine("Write Failed");
+               
+            }
+
+           
+        }
+
+        public static async Task UploadFileAsync(string @filePath)
+        {
+            try
+            {
+                _blobClient = _containerClient.GetBlobClient(Path.GetFileName(filePath));
+
+                using FileStream fileStream = File.OpenRead(filePath);
+                await _blobClient.UploadAsync(fileStream, true);
+                fileStream.Close();
+                
+            }
+            catch 
+            {
+                Console.WriteLine("Upload Failed");
+                
+            }
+
+           
+
+        }
+
+        public static async Task DownloadFileAsync(string @downloadPath)
+        {
+            try
+            {
+                if (!Directory.Exists(Path.GetDirectoryName(downloadPath)))
+                {
+                    Directory.CreateDirectory(Path.GetDirectoryName(downloadPath));
+
+                    BlobDownloadInfo download = await _blobClient.DownloadAsync();
+
+                    using FileStream fileStream = File.OpenWrite(downloadPath);
+
+                    await download.Content.CopyToAsync(fileStream);
+                    fileStream.Close();
+
+                    
+
+                }
+            }
+            catch 
+            {
+                Console.WriteLine("DownloadFail");
+                
+            }
+            
+           
+           
+        }
+
+        public static async Task<string>   ReadDownloadFileAsync(string @downloadPath)
+        {
+            try
+            {
+                return await File.ReadAllTextAsync(downloadPath);
+                
+            }
+            catch 
+            {
+                Console.WriteLine("ReadDownload Failed");
+                return "";
+               
+            }
+            
+
+        }
+    }
+}
